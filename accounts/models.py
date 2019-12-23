177e20e2acdc17a54a1e4d4cdc09719fc85d99f3
Filipe <ax-filipe@hotmail.com>
2019-12-23 06:12:25 -0400

    Evitando erro no postgres

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    id = models.BigIntegerField(primary_key = True)
+    id = models.BigIntegerField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth = models.DateField(null=True, blank=True)
 
